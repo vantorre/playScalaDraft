3f7d59da72636800bb42b4ca71f65335648889d0
fabien vantorre <fabien.vantorre@hotmail.fr>
2018-11-05 22:54:45 +0100

    myown page data ok

---
+++ app/controllers/MyOwnController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+import play.api.libs.json
+
+@Singleton
+class MyOwnController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+  extends AbstractController(cc) {
+
+  def myownPage = Action {
+    Ok(views.html.myown("Mon message"))
+  }
+
+  def myownData = Action {
+    Ok("5")
+  }
+
+
+}
