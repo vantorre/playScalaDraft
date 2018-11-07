2141d79ce9a0044540e80d12b751f4b9b8accd9e
ouikene koceila <koceila.ouikene@worldline.com>
2018-11-07 01:12:58 +0100

    First version of list of product works

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -12,9 +12,10 @@ class ProductController @Inject()(cc: ControllerComponents) (implicit assetsFind
 
   def all = Action {
     var p1 = new Product("produit1", 11);
-    //var p2 = new Product{name="produit2"; age=22};
-    var produits = List(p1);
+    var p2 = new Product("produit2",22);
+    var products = List(p1);
+    products = p2 :: products ;
 
-    Ok(views.html.produits(p1))
+    Ok(views.html.produits(products))
   }
 }
